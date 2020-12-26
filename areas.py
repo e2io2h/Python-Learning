c67be58c82be4b27a26eb8a0d4900cfa43f448df
e2io2h <h.elyot@gmail.com>
2020-12-26 22:02:49 +0100

    anothe change

--- areas.py
+++ areas.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import math
 
 def triangle(base, height):
@@ -10,3 +8,6 @@ def rectangle(base, height):
 
 def circle(radius):
     return math.pi*(radius**2)
+
+def donut(outside_radius, inside_radius):
+    return circle(outside_radius) - circle(inside_radius)
